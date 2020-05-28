e2928b9edad3caca0345960bf4cc098a7f23a678
Kiryl Parfiankou <kiryl.parfiankou@itechart-group.com>
2020-05-29 00:05:34 +0300

    module3-task1: bug fixing

--- src/express/routes/offers-routers.js
+++ src/express/routes/offers-routers.js
@@ -5,9 +5,9 @@ const {Router} = require(`express`);
 const offersRouter = new Router();
 
 
-offersRouter.get(`/category/:id`, (req, res) => res.send(`/category/:id`));
-offersRouter.get(`/add`, (req, res) => res.send(`/add`));
-offersRouter.get(`/edit/:id`, (req, res) => res.send(`/edit/:id`));
-offersRouter.get(`/:d`, (req, res) => res.send(`/:id`));
+offersRouter.get(`/category/:id`, (req, res) => res.send(`/offers/category/:id`));
+offersRouter.get(`/add`, (req, res) => res.send(`/offers/add`));
+offersRouter.get(`/edit/:id`, (req, res) => res.send(`/offers/edit/:id`));
+offersRouter.get(`/:id`, (req, res) => res.send(`/offers/:id`));
 
 module.exports = offersRouter;
