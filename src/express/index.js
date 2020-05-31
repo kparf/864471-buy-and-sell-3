9d9390810e04caf99cc171b94c7491c5f75e4384
Kiryl Parfiankou <kiryl.parfiankou@itechart-group.com>
2020-05-31 20:51:06 +0300

    module3-task2

--- src/express/index.js
+++ src/express/index.js
@@ -4,10 +4,18 @@ const express = require(`express`);
 const mainRoutes = require(`./routes/main-routes`);
 const myRoutes = require(`./routes/my-routers`);
 const offersRoutes = require(`./routes/offers-routers`);
+const path = require(`path`);
+
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 
 const app = express();
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
